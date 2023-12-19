a2384df21236120123be98adb65835e47d0718a5
ersandagdeviren <ersandagdeviren@gmail.com>
2023-12-19 15:01:43 +0300

    search is completed

--- Simetri/order/urls.py
+++ Simetri/order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name="order"
 urlpatterns = [
     path("", views.index, name="home"),
+    path("search/",views.search, name ="search")
 ]
--- Simetri/order/views.py
+++ Simetri/order/views.py
@@ -4,18 +4,23 @@ from django import forms
 class ProductSearchForm (forms.Form):
     product=forms.CharField(label="Search Product")
 
+def search(request):
+    if request.method=="POST":
+        form=ProductSearchForm( request.POST)
+        if form.is_valid():
+            product=form.cleaned_data["product"]
+            products=["tk3100","pk3010"]
+            if product in products:
+                return render(request,"order/index.html",{"form":form,"product":product.upper()})
+            else:
+                return render(request, "order/index.html",{"form":ProductSearchForm()})
+
+
+        
+
 # Create your views here.
 def index(request):
-    if request.method=="POST":
-        form=ProductSearchForm(request.POST)
-        if form.is_valid:
-            product=form.cleaned_data["product"]
-            products=["tk3100","pk3010","mpc2503"]
-            if product in products:
-                return render(request, "order/index.html",{"product" :product})
-            else:
-                return render(request, "order/index.html",{"form" :form})
-    return render(request, "order/index.html",{"product" :ProductSearchForm()})
+    return render(request, "order/index.html",{"form":ProductSearchForm()})
 
 
 
